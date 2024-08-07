ed05e25f979f8000aaf5e348da0a5ecb1590624c
Niroshan100 <tharinduniroshan016@gmail.com>
2024-08-07 13:51:59 +0530

    create text api router

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRouter from'./routes/user.route.js';
 
 const app =express();
 mongoose.connect(process.env.MONGO)
@@ -16,9 +17,22 @@ mongoose.connect(process.env.MONGO)
 
 
 
+
+app.get('/api/user/test', (req, res) => {
+    res.send('api router is working');
+  });
+  
+
+
+
+ 
+  
 app.listen(3000, () => {
 
     console.log('Server is running on port 3000!');
 }
 
-)
+);
+
+//api router
+app.use("/app/user",userRouter);
---
+++ api/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { test } from '../contrlloers/user.contoller.js';
+
+
+
+const router = express.Router();
+
+router.get('/test' ,test);
+
+export default router;
